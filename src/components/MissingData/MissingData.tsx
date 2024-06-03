2c59327ea17e23355859e0d5f0fafe5da2eff1b9
yechan <kevin2695@naver.com>
2024-06-03 22:31:57 +0900

    [task-13] missing data 알람 설정

--- src/components/MissingData/MissingData.tsx
+++ src/components/MissingData/MissingData.tsx
@@ -1,15 +1,30 @@
+import { MissingDataType } from '@src/interface/excel';
 import styles from './MissingData.module.scss';
 
-const data = {
-  text: '텍스트가 필수값인데 존재하지 않습니다.',
+const data: Record<MissingDataType, string> = {
+  objectType: 'objectType 이 유효하지 않습니다.',
+  text: 'content_text 값이 존재하지 않습니다.',
+  link: '"content_web_url" 또는 "content_mobile_web_url" 중 1개는 필수값 입니다.',
+  content:
+    '"content_title", "content_description" 또는 "content_image_url" 값 중 1개는 존재해야 합니다.',
+  regularPrice: 'regular_price 값이 존재하지 않거나 , 숫자가 아닌 값이 입력되었습니다.',
+  address: 'address 값이 존재하지 않습니다.',
+  headerTitle: 'header_title 값이 존재하지 않습니다.',
+  contents: 'contents 의 유효한 값이 2개 이상이어야 합니다.',
 };
 
-const MissingData = ({ missingData }: { missingData: Set<string> }) => {
+const MissingData = ({ missingData }: { missingData: Set<MissingDataType> }) => {
   if (!missingData) {
     return;
   }
 
-  return <div className={styles.wrapper}>{[...missingData].map((value) => value)}</div>;
+  return (
+    <div className={styles.wrapper}>
+      {[...missingData].map((value) => {
+        return <p>{data[value]}</p>;
+      })}
+    </div>
+  );
 };
 
 export default MissingData;
